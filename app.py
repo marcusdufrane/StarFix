fc0f9e2e70d6835ca73cc92c9f332d5ea5fc82c3
marshalium <marshall.scorcio@gmail.com>
2010-04-12 17:46:52 -0500

    Add a textentry box to the UI.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from bottle import route, run, request, view
+from StringIO import StringIO
 
 import parser
 
@@ -9,18 +10,21 @@ import parser
 def index():
     return {}
 
-
 @route('/result', method="POST")
 @view('result')
 def template_result():
-    star_file = request.POST.get('starfile').file
+    if request.POST.get('startext'):
+        star_file = StringIO(request.POST.get('startext'))
+    else:    
+        star_file = request.POST.get('starfile').file
+    
     star = parser.parse_star(star_file)
     
     context = {}
     context['name'] = star.name
     context['sections'] = star.sections
     return context
-
+    
 
 run(host='localhost', port=8080)
 
--- views/index.tpl
+++ views/index.tpl
@@ -5,11 +5,17 @@
   </head>
   <body>
     <h1>StarFix</h1>
-    <p>Upload the STAR to be beautified</p>
+    <p>Upload the STAR to be beautified!</p>
+    
     <form action="/result" method="post" enctype="multipart/form-data">
+      <h2>Paste it here!<h2>
+      <textarea name="startext" rows="12" cols="70"></textarea>
+      
+      <h2>Or upload a text file here!</h2>
       <input name="starfile" type="file" />
       <br /><br />
-      <input name="submit" type="submit" value="Beutify!" />
+      
+      <input name="submit" type="submit" value="Beautify!" />
     </form>
   </body>
 </html>
